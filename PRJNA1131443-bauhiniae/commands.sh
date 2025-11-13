7a79a2fda24fa8bd264540ed44ac387287927bf3
David Studholme <d.j.studholme@exeter.ac.uk>
2025-11-13 19:14:41 +0000

    Ran Unicycler on NCPPB1629

--- PRJNA1131443-bauhiniae/commands.sh
+++ PRJNA1131443-bauhiniae/commands.sh
@@ -1,5 +1,6 @@
 ./fastq-dump --gzip --split-files SRR30035987 SRR29695112 SRR29695113
 
+trim_galore -q 30 --paired SRR29695112_1.fastq.gz SRR29695112_2.fastq.gz 
 
 conda create -n hybracter_env python=3.11
 conda activate hybracter_env
@@ -18,10 +19,11 @@ conda create -n unicycler_env
 conda activate unicycler_env
 conda install bioconda::unicycler
 
-conda list -n unicycler_env > hybracter_env_packages.txt
+conda list -n unicycler_env > unicycler_env_packages.txt
 conda env export > unicycler_env.yaml
 
-unicycler -1 SRR29695112_1.fastq.gz -2 SRR29695112_2.fastq.gz -o NCPPB1629.unicycler
+
+unicycler -1 SRR29695112_1_val_1.fq.gz -2 SRR29695112_2_val_2.fq.gz -o NCPPB1629.unicycler
 
 
 
