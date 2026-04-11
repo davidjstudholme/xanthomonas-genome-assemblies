63ec929a6fec863ba07e0576f02144fbebddc10a
David Studholme <d.j.studholme@exeter.ac.uk>
2026-04-11 16:32:49 +0000

    Assembled WHRI_8978 with Unicycler

--- PRJNA988133-Xcc/commands.sh
+++ PRJNA988133-Xcc/commands.sh
@@ -11,7 +11,6 @@ unicycler -1 SRR25205310_1_val_1.fq.gz -2 SRR25205310_2_val_2.fq.gz -o WHRI_3873
 unicycler -1 SRR25205311_1_val_1.fq.gz -2 SRR25205311_2_val_2.fq.gz -o WHRI_3851A.unicycler
 unicycler -1 SRR25205314_1_val_1.fq.gz -2 SRR25205314_2_val_2.fq.gz -o WHRI_738C.unicycler
 
-
 ./fastq-dump --gzip --split-files SRR27397680 SRR25205263
 ./fastq-dump --gzip --split-files SRR27397677 SRR25183103
 ./fastq-dump --gzip --split-files SRR27397676 SRR25183094
@@ -29,3 +28,12 @@ conda list -n hybracter_env > hybracter_env_packages.txt
 conda env export > hybracter_env.yaml
 
 hybracter hybrid -i hybracter.csv -o hybracter-hybrid -t 12
+
+#conda create -n unicycler_env                                                                                                                                           
+conda activate unicycler_env
+#conda install bioconda::unicycler                                                                                                                                       
+conda list -n unicycler_env > unicycler_env_packages.txt
+conda env export > unicycler_env.yaml
+
+unicycler -1 SRR25183094_1_val_1.fq.gz -2 SRR25183094_2_val_2.fq.gz -l SRR27397676_1.fastq.gz -o WHRI_8978.unicycler
+
